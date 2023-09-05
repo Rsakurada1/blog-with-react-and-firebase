260909208e2ab5f00649d902fbb4ac0fe231b9e1
Rikito Sakurada <rikito82k@gmail.com>
2023-09-06 01:01:29 +0900

    key

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -23,7 +23,7 @@ useEffect(() => {
     <div className='homePage'>
       {postList.map((post) => {
         return (
-          <div className='postContents'>
+          <div className='postContents' key={post.id}>
         <div className='postHeader'>
           <h1>{post.title}</h1>
         </div>
